232fd8896f4a38ac46933ecaeb346c049ef92625
Rob <r4226@pm.me>
2024-04-08 06:25:45 -0400

    Fix double "x" in close modal button
    The close modal button currently has two 'X's on top of each other.
    Bootstrap provides the X as a properly sized SVG via the CSS background
    property so the 'x' within the HTML <button> is not needed.

--- assets/src/js/parts/file_preview_modal.js
+++ assets/src/js/parts/file_preview_modal.js
@@ -10,7 +10,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title"></h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                         </div>
