3c67b0d6cabad457566cd93b2c077f804f5cad4f
Diego <diego@redondo.ca>
2024-09-11 00:32:42 -0700

    Fix second typo

--- includes/functions.assets.php
+++ includes/functions.assets.php
@@ -21,7 +21,7 @@ function get_asset_locations()
 
 function get_asset_positions()
 {
-    $poitions = [
+    $positions = [
         'head' => __('In <head>'),
         'body_top' => __('Before <body>'),
         'body_bottom' => __('After </body>'),
@@ -33,7 +33,7 @@ function get_asset_positions()
     ], [
         '&lt',
         '&gt',
-    ], $poitions);
+    ], $positions);
 }
 
 function format_asset_language_name($name)
